22a164f2045dc6dc98db9e7cb127f74a93c140e4
frontpro <aly.arbab@gmail.com>
2022-09-20 09:46:48 +0200

    more than parent design

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const parents = document.querySelector(".parents");
-const btn = document.querySelector(".question-btn");
+// const parents = document.querySelector(".parents");
+// const btn = document.querySelector(".question-btn");
 
-btn.addEventListener('click',function(){
-    parents.classList.toggle("show-child");
+// btn.addEventListener('click',function(){
+//     parents.classList.toggle("show-child");
+// })
+
+// traversing the dom
+// const parents = document.querySelector(".parents");
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         const parents = e.currentTarget.parentElement.parentElement.parentElement;
+//         parents.classList.toggle("show-child");   
+//     })
+// })
+
+//using selectors inside the element
+const parents = document.querySelectorAll(".parents");
+parents.forEach(function(parent){
+    const btn = parent.querySelector(".question-btn");
+    btn.addEventListener('click',function(){
+        parent.classList.toggle("show-child");
+    })
 })
